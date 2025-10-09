ceae0b7b1d9a9100b835f22709bb567e2c0cb0b9
baljinder1994 <149044680+baljinder1994@users.noreply.github.com>
2025-10-09 19:01:01 +0530

    Update App.jsx

--- Project6/src/App.jsx
+++ Project6/src/App.jsx
@@ -1,34 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const[word,setWord]=useState("")
+  const[error,setError]=useState("")
+  const[data,setData]=useState(null)
 
+  const fetchMeaning= async()=>{
+    if(!word.trim()) return
+
+    setError("")
+    setData(null)
+    try{
+      const res= await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      if(!res.ok) throw new Error("Word not found")
+        const result= await res.json()
+      setData(result[0])
+    }catch(err){
+      setError("No definition found")
+    }
+  }
+
+  const playAudio=(url)=>{
+    const audio= new Audio(url)
+    audio.play()
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='min-h-screen bg-gradient-to-br from-blue-100 to-purple-100 flex flex-col items-center justify-center p-6'>
+      <div className='bg-white/80 backdrop-blur-lg shadow-2xl rounded-2xl p-8 w-full max-w-xl'>
+        <h1 className='text-3xl font-bold text-center'>Smart Dictionary</h1>
+     
+      <div className='flex gap-2 mb-6'>
+        <input type="text"
+        placeholder='Enter a word(e.g. hello)'
+        value={word}
+        onChange={(e) => setWord(e.target.value)}
+        className='flex-1 border border-gray-500 rounded-xl p-3 '
+        ></input>
+
+        <button 
+        className='bg-indigo-600 text-white px-5 rounded-xl'
+        onClick={fetchMeaning}>Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+
+{error &&(
+ <p>{error} </p>
+)}
+{data &&(
+     
+     <div className='text-gray-800'>
+      <div className='flex justify-between items-center mb-2'>
+        <h2 className='text-2xl font-bold capitalize'>{data.word}</h2>
+        {data.phonetics?.some(p => p.audio) ?(
+          <button
+          onClick={() =>{
+            const audioUrl=data.phonetics.find(p => p.audio)?.audio;
+            if(audioUrl){
+              const audio=new Audio(audioUrl)
+              audio.play()
+            }else{
+              alert("No pronounciation av.")
+            }
+          }}
+          className='bg-blue-100  p-2 rounded-full'
+          >🔊</button>
+        ):(
+          <button >🔇</button>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <p className='text-indigo-500 text-lg italic'>{data.phonetic || "_"}</p>
+
+      <div className='mt-4 space-y-4'>
+        {data.meanings.map((meaning,idx) =>(
+          <div className='bg-gray-50 border-l-4 border-indigo-400 p-3 rounded-xl'>
+            <p className='font-bold text-indigo-600'>{meaning.partOfSpeech}</p>
+            {meaning.definitions.map((def,i) =>(
+              <div className='mt-2'>
+                <p className='text-gray-700'><span className='font-semibold'>Definition:</span>{" "}
+                {def.definition}
+                </p>
+                {def.example &&(
+                  <p className='text-sm text-gray-500 italic mt-1'>"{def.example}"</p>
+                )}
+              </div>
+            ))}
+          
+          </div>
+        ))}
+      </div>
+     </div>
+     )}
+      </div>
+    </div>
   )
 }
 
